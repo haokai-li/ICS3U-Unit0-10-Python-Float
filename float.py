9b05cf0a3a2467d0e6ae50b726563984769d0ab5
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-29 01:14:20 +0000

    float

---
+++ float.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This Program is about float
+
+
+def main():
+    # This function is about float
+
+    # input
+    print("IT is about float of (0.1 + 0.2)")
+
+    # process
+    answer = 0.1 + 0.2
+
+    # output
+    print("(0.1 + 0.2) = {}.".format(answer))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
